fcdfa3a3c77db31e48435ee152f0425bfe6a293c
Andy Guinther <andrew.guinther@gmail.com>
2014-12-08 21:12:49 -0700

    added production bundling for heroku

--- spec/features/creating_products_spec.rb
+++ spec/features/creating_products_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 feature 'Product Management' do
+
   scenario 'user creates a new product' do
+
     visit new_product_path
 
     fill_in 'Name', with: 'Apple'
@@ -11,12 +13,12 @@ feature 'Product Management' do
 
     click_button 'Create Product'
 
-    expect(page).to have_text(
-      'Product was successfully created'
-    )
+    expect(page).to have_text('Product was successfully created')
+
+
   end
 
-  scenario 'user edits an existing product' do
+  scenario 'user edits existing product' do
     product = FactoryGirl.create(:product)
 
     visit edit_product_path(product)
@@ -25,11 +27,63 @@ feature 'Product Management' do
 
     click_button 'Update Product'
 
-    expect(page).to have_text(
-      'Product was successfully updated'
-    )
-    expect(page).to have_text(
-      'Apple'
-    )
+    expect(page).to have_text('Product was successfully updated')
+
+    expect(page).to have_text('Apple')
+
   end
+
+  scenario 'user views a product' do
+    product = FactoryGirl.create(:product)
+
+    visit product_path(product)
+
+    expect(page).to have_text(product.name)
+    expect(page).to have_text(product.description)
+    expect(page).to have_text(product.price)
+    expect(page).to have_image(product.image_url.downcase)
+
+  end
+
+  scenario 'user views a list of products' do
+
+    product1 = FactoryGirl.create(:product)
+    product2 = FactoryGirl.create(:product)
+
+    visit products_path
+
+    expect(page).to have_text(product1.name)
+    expect(page).to have_text(product1.description)
+    expect(page).to have_text(product1.price)
+    expect(page).to have_image(product1.image_url.downcase)
+
+    expect(page).to have_text(product2.name)
+    expect(page).to have_text(product2.description)
+    expect(page).to have_text(product2.price)
+    expect(page).to have_image(product2.image_url.downcase)
+
+
+
+  end
+
+  scenario 'user deletes a product' do
+
+    product = FactoryGirl.create(:product)
+
+    visit products_path
+
+    within "#product_#{product.id}" do
+      click_link 'Destroy'
+    end
+
+    expect(page).to have_text("Product was successfully destroyed.")
+    expect(page).to_not have_text(product.name)
+    expect(page).to_not have_text(product.description)
+    expect(page).to_not have_text(product.price)
+    expect(page).to_not have_image(product.image_url.downcase)
+
+
+  end
+
+
 end
